7491ee3321a9ec97b36783b0111339fa3270200b
saurab <117426794+41saurab@users.noreply.github.com>
2024-02-06 22:53:59 +0545

    Add files via upload

---
+++ she/script.js
@@ -0,0 +1,13 @@
+function yes() {
+  const image = document.getElementById("vimage");
+  const imge = "https://i.pinimg.com/originals/cd/0d/78/cd0d783e2a0bbd1eff091168de9ffe14.gif";
+  alert("You are his now.");
+  image.src = imge;
+}
+
+function no() {
+  const image = document.getElementById("vimage");
+  const imge = "https://i.pinimg.com/originals/ec/0a/4e/ec0a4e9238a76a2b9afd367a98c14c32.gif";
+  alert("You don't love him much.");
+  image.src = imge;
+}
